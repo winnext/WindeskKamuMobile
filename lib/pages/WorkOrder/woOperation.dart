e648cfd94be0eca6cd7868335a0eb116d7d01362
MertSaygili <mert71719601@gmail.com>
2023-07-05 14:56:20 +0300

    code cleaned

--- lib/pages/WorkOrder/woOperation.dart
+++ lib/pages/WorkOrder/woOperation.dart
@@ -65,191 +65,203 @@ class _WoOperationState extends State<WoOperation> {
           sectionOpeningHapticFeedback: SectionHapticFeedback.heavy,
           sectionClosingHapticFeedback: SectionHapticFeedback.light,
           children: [
-            AccordionSection(
-              isOpen: false,
-              headerBackgroundColor: APPColors.Login.blue,
-              leftIcon: const Icon(Icons.insights_rounded, color: Colors.white),
-              header: Text('Eforlar', style: _headerStyle),
-              contentBorderColor: const Color(0xffffffff),
-              headerBackgroundColorOpened: Colors.black,
-              content: Accordion(
-                maxOpenSections: 1,
-                headerBackgroundColorOpened: Colors.black54,
-                headerPadding: const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
-                children: [
-                  AccordionSection(
-                    isOpen: false,
-                    leftIcon: const Icon(Icons.add, color: Colors.white),
-                    headerBackgroundColor: Colors.green,
-                    headerBackgroundColorOpened: Colors.black,
-                    header: Text('Efor Ekle', style: _headerStyle),
-                    content: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                      ),
-                      onPressed: () {
-                        setState(() {});
-
-                        showModalBottomSheet<void>(
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.only(
-                              topLeft: Radius.circular(20.0),
-                              topRight: Radius.circular(20.0),
-                            ),
-                          ),
-                          isScrollControlled: true,
-                          context: context,
-                          builder: (BuildContext context) {
-                            return WoEforts(woCode: widget.woCode);
-                          },
-                        );
-                      },
-                      child: const Text('Yeni Efor Ekle'),
-                    ),
-                    contentHorizontalPadding: 20,
-                    contentBorderColor: Colors.black54,
-                  ),
-                  AccordionSection(
-                    isOpen: false,
-                    leftIcon: const Icon(Icons.compare_rounded, color: Colors.white),
-                    header: Text('Eforlar', style: _headerStyle),
-                    headerBackgroundColor: APPColors.Login.blue,
-                    headerBackgroundColorOpened: Colors.black,
-                    contentBorderColor: Colors.black54,
-                    content: DataTable(
-                      sortAscending: true,
-                      sortColumnIndex: 1,
-                      dataRowHeight: 40,
-                      showBottomBorder: true,
-                      columns: [
-                        DataColumn(label: Text('Kullanıcı', style: _contentStyleHeader), numeric: false),
-                        DataColumn(label: Text('Süre', style: _contentStyleHeader)),
-                        DataColumn(
-                          label: Text('Sil', style: _contentStyleHeader),
-                        ),
-                      ],
-                      rows: [
-                        for (var i = 0; i < woDetailViewProvider.eforlarArray[1].length; i++)
-                          DataRow(
-                            cells: [
-                              DataCell(Text(woDetailViewProvider.eforlarArray[1][i], style: _contentStyle, textAlign: TextAlign.right)),
-                              DataCell(Text(woDetailViewProvider.eforlarArray[2][i], style: _contentStyle)),
-                              DataCell(GestureDetector(
-                                onTap: () {
-                                  woDetailViewProvider.deleteEffort(context, woDetailViewProvider.eforlarArray[0][i], widget.woCode);
-                                },
-                                child: const Icon(Icons.delete, color: Colors.red),
-                              ))
-                            ],
-                          ),
-                        //  TextButton(onPressed: (){woDetailViewProvider.deleteEffort(context,woDetailViewProvider.eforlarArray[0][i],widget.woCode );  }, child: Center(child: Text('X',style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),)))
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            AccordionSection(
-              isOpen: false,
-              headerBackgroundColor: APPColors.Login.blue,
-              leftIcon: const Icon(Icons.warehouse, color: Colors.white),
-              header: Text('Malzemeler', style: _headerStyle),
-              contentBorderColor: const Color(0xffffffff),
-              headerBackgroundColorOpened: Colors.black,
-              content: Accordion(
-                maxOpenSections: 1,
-                headerBackgroundColorOpened: Colors.black54,
-                headerPadding: const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
-                children: [
-                  AccordionSection(
-                    isOpen: false,
-                    leftIcon: const Icon(Icons.add, color: Colors.white),
-                    headerBackgroundColor: Colors.green,
-                    headerBackgroundColorOpened: Colors.black,
-                    header: Text('Malzeme Ekle', style: _headerStyle),
-                    content: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.green,
-                        shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                      ),
-                      onPressed: () async {
-                        await woDetailViewProvider.getStore();
-
-                        showModalBottomSheet<void>(
-                          // context and builder are
-                          // required properties in this widget
-                          context: context,
-                          builder: (BuildContext context) {
-                            // we set up a container inside which
-                            // we create center column and display text
-
-                            // Returning SizedBox instead of a Container
-                            return WoSpareParts(
-                              woCode: widget.woCode,
-                            );
-                          },
-                        );
-                      },
-                      child: const Text('Yeni Malzeme Ekle'),
-                    ),
-                    contentHorizontalPadding: 20,
-                    contentBorderColor: Colors.black54,
-                  ),
-                  AccordionSection(
-                    isOpen: false,
-                    leftIcon: const Icon(Icons.warehouse, color: Colors.white),
-                    header: Text('Malzemeler', style: _headerStyle),
-                    headerBackgroundColor: APPColors.Login.blue,
-                    headerBackgroundColorOpened: Colors.black,
-                    contentBorderColor: Colors.black54,
-                    content: DataTable(
-                      sortAscending: true,
-                      sortColumnIndex: 1,
-                      dataRowHeight: 40,
-                      showBottomBorder: true,
-                      columns: [
-                        DataColumn(label: Text('Kullanıcı', style: _contentStyleHeader), numeric: false),
-                        DataColumn(label: Text('Süre', style: _contentStyleHeader)),
-                        DataColumn(
-                          label: Text('Sil', style: _contentStyleHeader),
-                        ),
-                      ],
-                      rows: [
-                        for (var i = 0; i < woDetailViewProvider.eforlarArray[1].length; i++)
-                          DataRow(
-                            cells: [
-                              DataCell(Text(woDetailViewProvider.eforlarArray[1][i], style: _contentStyle, textAlign: TextAlign.right)),
-                              DataCell(Text(woDetailViewProvider.eforlarArray[2][i], style: _contentStyle)),
-                              DataCell(GestureDetector(
-                                onTap: () {
-                                  woDetailViewProvider.deleteEffort(context, woDetailViewProvider.eforlarArray[0][i], widget.woCode);
-                                },
-                                child: const Icon(Icons.delete, color: Colors.red),
-                              ))
-                            ],
-                          ),
-                        //  TextButton(onPressed: (){woDetailViewProvider.deleteEffort(context,woDetailViewProvider.eforlarArray[0][i],widget.woCode );  }, child: Center(child: Text('X',style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),)))
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            _efortsAccordionSection(context, woDetailViewProvider),
+            _materialsAccordionSection(woDetailViewProvider, context),
             _personalsAccordionSection(woDetailViewProvider, context),
             _documantsPhotosAccordionSection(woDetailViewProvider, context),
-            AccordionSection(
-              isOpen: false,
-              leftIcon: const Icon(Icons.contact_page, color: Colors.white),
-              header: Text(_instructions, style: _headerStyle),
-              content: Wrap(
-                children: List.generate(30, (index) => const Icon(Icons.contact_page, size: 30, color: Color(0xff999999))),
-              ),
-            ),
+            _instructionsAccordionSection(),
           ],
         ),
       );
     });
+  }
+
+  AccordionSection _instructionsAccordionSection() {
+    return AccordionSection(
+      isOpen: false,
+      leftIcon: const Icon(Icons.contact_page, color: Colors.white),
+      header: Text(_instructions, style: _headerStyle),
+      content: Wrap(
+        children: List.generate(30, (index) => const Icon(Icons.contact_page, size: 30, color: Color(0xff999999))),
+      ),
+    );
+  }
+
+  AccordionSection _materialsAccordionSection(WoDetailViewProvider woDetailViewProvider, BuildContext context) {
+    return AccordionSection(
+      isOpen: false,
+      headerBackgroundColor: APPColors.Login.blue,
+      leftIcon: const Icon(Icons.warehouse, color: Colors.white),
+      header: Text('Malzemeler', style: _headerStyle),
+      contentBorderColor: const Color(0xffffffff),
+      headerBackgroundColorOpened: Colors.black,
+      content: Accordion(
+        maxOpenSections: 1,
+        headerBackgroundColorOpened: Colors.black54,
+        headerPadding: const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+        children: [
+          AccordionSection(
+            isOpen: false,
+            leftIcon: const Icon(Icons.add, color: Colors.white),
+            headerBackgroundColor: Colors.green,
+            headerBackgroundColorOpened: Colors.black,
+            header: Text('Malzeme Ekle', style: _headerStyle),
+            content: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green,
+                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+              ),
+              onPressed: () async {
+                await woDetailViewProvider.getStore();
+
+                showModalBottomSheet<void>(
+                  // context and builder are
+                  // required properties in this widget
+                  context: context,
+                  builder: (BuildContext context) {
+                    // we set up a container inside which
+                    // we create center column and display text
+
+                    // Returning SizedBox instead of a Container
+                    return WoSpareParts(
+                      woCode: widget.woCode,
+                    );
+                  },
+                );
+              },
+              child: const Text('Yeni Malzeme Ekle'),
+            ),
+            contentHorizontalPadding: 20,
+            contentBorderColor: Colors.black54,
+          ),
+          AccordionSection(
+            isOpen: false,
+            leftIcon: const Icon(Icons.warehouse, color: Colors.white),
+            header: Text('Malzemeler', style: _headerStyle),
+            headerBackgroundColor: APPColors.Login.blue,
+            headerBackgroundColorOpened: Colors.black,
+            contentBorderColor: Colors.black54,
+            content: DataTable(
+              sortAscending: true,
+              sortColumnIndex: 1,
+              dataRowHeight: 40,
+              showBottomBorder: true,
+              columns: [
+                DataColumn(label: Text('Kullanıcı', style: _contentStyleHeader), numeric: false),
+                DataColumn(label: Text('Süre', style: _contentStyleHeader)),
+                DataColumn(
+                  label: Text('Sil', style: _contentStyleHeader),
+                ),
+              ],
+              rows: [
+                for (var i = 0; i < woDetailViewProvider.eforlarArray[1].length; i++)
+                  DataRow(
+                    cells: [
+                      DataCell(Text(woDetailViewProvider.eforlarArray[1][i], style: _contentStyle, textAlign: TextAlign.right)),
+                      DataCell(Text(woDetailViewProvider.eforlarArray[2][i], style: _contentStyle)),
+                      DataCell(GestureDetector(
+                        onTap: () {
+                          woDetailViewProvider.deleteEffort(context, woDetailViewProvider.eforlarArray[0][i], widget.woCode);
+                        },
+                        child: const Icon(Icons.delete, color: Colors.red),
+                      ))
+                    ],
+                  ),
+                //  TextButton(onPressed: (){woDetailViewProvider.deleteEffort(context,woDetailViewProvider.eforlarArray[0][i],widget.woCode );  }, child: Center(child: Text('X',style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),)))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  AccordionSection _efortsAccordionSection(BuildContext context, WoDetailViewProvider woDetailViewProvider) {
+    return AccordionSection(
+      isOpen: false,
+      headerBackgroundColor: APPColors.Login.blue,
+      leftIcon: const Icon(Icons.insights_rounded, color: Colors.white),
+      header: Text('Eforlar', style: _headerStyle),
+      contentBorderColor: const Color(0xffffffff),
+      headerBackgroundColorOpened: Colors.black,
+      content: Accordion(
+        maxOpenSections: 1,
+        headerBackgroundColorOpened: Colors.black54,
+        headerPadding: const EdgeInsets.symmetric(vertical: 7, horizontal: 15),
+        children: [
+          AccordionSection(
+            isOpen: false,
+            leftIcon: const Icon(Icons.add, color: Colors.white),
+            headerBackgroundColor: Colors.green,
+            headerBackgroundColorOpened: Colors.black,
+            header: Text('Efor Ekle', style: _headerStyle),
+            content: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+              ),
+              onPressed: () {
+                setState(() {});
+
+                showModalBottomSheet<void>(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20.0),
+                      topRight: Radius.circular(20.0),
+                    ),
+                  ),
+                  isScrollControlled: true,
+                  context: context,
+                  builder: (BuildContext context) {
+                    return WoEforts(woCode: widget.woCode);
+                  },
+                );
+              },
+              child: const Text('Yeni Efor Ekle'),
+            ),
+            contentHorizontalPadding: 20,
+            contentBorderColor: Colors.black54,
+          ),
+          AccordionSection(
+            isOpen: false,
+            leftIcon: const Icon(Icons.compare_rounded, color: Colors.white),
+            header: Text('Eforlar', style: _headerStyle),
+            headerBackgroundColor: APPColors.Login.blue,
+            headerBackgroundColorOpened: Colors.black,
+            contentBorderColor: Colors.black54,
+            content: DataTable(
+              sortAscending: true,
+              sortColumnIndex: 1,
+              dataRowHeight: 40,
+              showBottomBorder: true,
+              columns: [
+                DataColumn(label: Text('Kullanıcı', style: _contentStyleHeader), numeric: false),
+                DataColumn(label: Text('Süre', style: _contentStyleHeader)),
+                DataColumn(
+                  label: Text('Sil', style: _contentStyleHeader),
+                ),
+              ],
+              rows: [
+                for (var i = 0; i < woDetailViewProvider.eforlarArray[1].length; i++)
+                  DataRow(
+                    cells: [
+                      DataCell(Text(woDetailViewProvider.eforlarArray[1][i], style: _contentStyle, textAlign: TextAlign.right)),
+                      DataCell(Text(woDetailViewProvider.eforlarArray[2][i], style: _contentStyle)),
+                      DataCell(GestureDetector(
+                        onTap: () {
+                          woDetailViewProvider.deleteEffort(context, woDetailViewProvider.eforlarArray[0][i], widget.woCode);
+                        },
+                        child: const Icon(Icons.delete, color: Colors.red),
+                      ))
+                    ],
+                  ),
+                //  TextButton(onPressed: (){woDetailViewProvider.deleteEffort(context,woDetailViewProvider.eforlarArray[0][i],widget.woCode );  }, child: Center(child: Text('X',style: TextStyle(color: Colors.red,fontWeight: FontWeight.bold),)))
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   AccordionSection _documantsPhotosAccordionSection(WoDetailViewProvider woDetailViewProvider, BuildContext context) {
@@ -475,11 +487,4 @@ class _WoOperationState extends State<WoOperation> {
       ),
     );
   }
-}
-
-class Data {
-  final String title;
-  final String value;
-
-  Data(this.title, this.value);
 }
